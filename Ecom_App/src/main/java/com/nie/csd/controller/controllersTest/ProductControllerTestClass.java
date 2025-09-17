2ecef69779fa1d38bb8cb62dc7007a1bb8d4022d
syeda-mizba <mizbaaasyedaaa@gmail.com>
2025-09-17 15:36:09 +0530

    2nd day updated code

--- Ecom_App/src/main/java/com/nie/csd/controller/controllersTest/ProductControllerTestClass.java
+++ Ecom_App/src/main/java/com/nie/csd/controller/controllersTest/ProductControllerTestClass.java
@@ -2,16 +2,19 @@ package com.nie.csd.controller.controllersTest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.nie.csd.controller.ProductController;
-@SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(ProductController.class)
 public class ProductControllerTestClass {
     @Autowired
     ProductController controller;
+    @Autowired
+    MockMvc mockMvc;
 
-    public void testSayHello(){
-        String result= controller.sayHello();
-        String expected="Hello from Product Controller";
-        assert(result.equals(expected));
+    public void testSayHello()throws Exception{
+        mockMvc.perform(get("/hello")).andExpect(status().isOk())
+        .andExpect(content().string("HELLO"));
     }
     
 }
